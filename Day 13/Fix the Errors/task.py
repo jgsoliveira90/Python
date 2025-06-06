25f452ff6491b26e9237a80ed972e84fa3a5e109
João Gabriel da Silva Oliveira <32915350+jgsoliveira90@users.noreply.github.com>
2025-06-06 11:44:13 -0300

    Dia 13

--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,7 @@
-age = int(input("How old are you?"))
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("Invalid number, Try again!")
+    age = int(input("How old are you?"))
 if age > 18:
-print("You can drive at age {age}.")
+    print(f"You can drive at age {age}.")
